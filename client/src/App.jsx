d22c571039dfcb66b335151c13d005a820fe42d4
Pawan <pawanb78@gmail.com>
2024-02-29 07:24:10 +0000

    react installed in the client along with all the required dependencies,(react-router-dom, toastify,react cookie, axios )

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import {Route, Routes} from 'react-router-dom';
+import {Home, Login, SignUp} from './Pages/Index';
+
+function App() {
+  return (
+    <>
+      <Routes>
+          <Route path='/' element = {<Home/>}></Route>
+          <Route path='/login' element = {<Login/>}></Route>
+          <Route path='/signup' element = {<SignUp/>}></Route>
+
+      </Routes>
+    </>
+  )
+}
+
+export default App
