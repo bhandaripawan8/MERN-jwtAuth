83dfbb9a2ec471d09c1703c94986a44806f41f91
BHANDARI <S2119656@glos.ac.uk>
2024-03-01 14:35:30 +0000

    login signup and home done

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -31,7 +31,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:4000/login",
+        "http://localhost:5000/login",
         {
           ...inputValue,
         },
