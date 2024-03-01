be70902385c36fab65eaf51247005e1cbdf865f2
BHANDARI <S2119656@glos.ac.uk>
2024-03-01 14:25:48 +0000

    login signup and home done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import './index.css'
 import {Route, Routes} from 'react-router-dom';
 import {Home, Login, SignUp} from './Pages/Index';
 
@@ -9,10 +10,8 @@ function App() {
           <Route path='/' element = {<Home/>}></Route>
           <Route path='/login' element = {<Login/>}></Route>
           <Route path='/signup' element = {<SignUp/>}></Route>
-
       </Routes>
     </>
   )
 }
-
 export default App
